f8ad6edcb285a0ada1c30151679aa7cdf4076749
WillKoehrsen <will@cortexintel.com>
2024-06-11 10:40:34 -0400

    Add database connection

---
+++ backend/main/database.py
@@ -0,0 +1,37 @@
+import os
+
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from gs_demo.main.config import settings
+
+
+def get_engine():
+    application_name = "gs_demo_development"
+    if settings.production:
+        application_name = "gs_demo_production"
+
+    ssl_args = {
+        "ssl": "require",
+        "server_settings": {"application_name": application_name},
+    }
+    DB_URL = f"postgresql+asyncpg://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_database}"  # noqa
+
+    # set echo to True to log all SQL statements
+    log_sql = os.getenv("LOG_SQL", "").lower() in ["true", "1"]
+
+    return create_async_engine(DB_URL, echo=log_sql, connect_args=ssl_args)
+
+
+def get_async_session():
+    engine = get_engine()
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+    return async_session
+
+
+async def get_db():
+    timeout = os.environ.get("STATEMENT_TIMEOUT", "180s")
+    async with get_async_session() as session:
+        # Set a timeout for all statements in this session
+        await session.execute(text(f"SET statement_timeout TO '{timeout}'"))
+        yield session
--- backend/main/main.py
+++ backend/main/main.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from fastapi import FastAPI
 
