9b0227bf5d6223a62eb3b7204d167fd58495d7e8
Max Kanter <kmax12@gmail.com>
2024-06-14 12:57:00 -0500

    Update table formatting and useData hook

--- frontend/src/hooks/useData.jsx
+++ frontend/src/hooks/useData.jsx
@@ -1,18 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchData = async ({ queryKey }) => {
-  const [{ tableName, limit }] = queryKey;
+const fetchData = async ({ tableName, limit }) => {
   const response = await axios.get(`${import.meta.env.VITE_API_HOST}/data`, {
     params: { table_name: tableName, limit },
   });
   return response.data;
 };
 
-const useData = (tableName, limit) => {
+const useData = ({ tableName, limit, enabled = true }) => {
   return useQuery({
     queryKey: ["data", { tableName, limit }],
-    queryFn: fetchData,
+    queryFn: () => fetchData({ tableName, limit }),
+    enabled,
   });
 };
 
