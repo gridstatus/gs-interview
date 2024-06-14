919e76ddcb503e4d411b13b7d071f1e1d4b77140
Max Kanter <kmax12@gmail.com>
2024-06-14 12:56:48 -0500

    remove unused stylesheets

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,12 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MantineProvider } from "@mantine/core";
 
-import "react-grid-layout/css/styles.css";
-import "react-resizable/css/styles.css";
-
 import "@mantine/core/styles.css";
-import "@mantine/dates/styles.css";
-import "@mantine/notifications/styles.css";
 
 import App from "./App";
 
